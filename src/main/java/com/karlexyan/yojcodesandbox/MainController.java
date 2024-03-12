a7e78177ab44b2318e9ff51a1fcceaadb9b67439
KarlexYan <karlexyan@aliyun.com>
2024-03-13 00:21:19 +0800

    [init] 编写测试接口，验证项目能够正常启动

---
+++ src/main/java/com/karlexyan/yojcodesandbox/MainController.java
@@ -0,0 +1,15 @@
+package com.karlexyan.yojcodesandbox;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "ok";
+    }
+}
