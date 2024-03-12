abb0ba5ba4ac0201609821d1a2813af189a0448b
KarlexYan <karlexyan@aliyun.com>
2024-03-13 00:05:38 +0800

    [init] 项目初始化

---
+++ src/main/java/com/karlexyan/yojcodesandbox/YojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.karlexyan.yojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YojCodeSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/karlexyan/yojcodesandbox/YojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karlexyan.yojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
