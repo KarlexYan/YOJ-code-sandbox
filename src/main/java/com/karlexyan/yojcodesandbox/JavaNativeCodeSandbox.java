4e9d826cb406d04190cf885224088fad01558d0c
KarlexYan <karlexyan@aliyun.com>
2024-03-14 22:36:58 +0800

    [feat] 封装工具类ProcessUtils 方便复用

--- src/main/java/com/karlexyan/yojcodesandbox/JavaNativeCodeSandbox.java
+++ src/main/java/com/karlexyan/yojcodesandbox/JavaNativeCodeSandbox.java
@@ -4,11 +4,10 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.lang.UUID;
 import com.karlexyan.yojcodesandbox.model.ExecuteCodeRequest;
 import com.karlexyan.yojcodesandbox.model.ExecuteCodeResponse;
+import com.karlexyan.yojcodesandbox.model.ExecuteMessage;
+import com.karlexyan.yojcodesandbox.utils.ProcessUtils;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
@@ -42,45 +41,12 @@ public class JavaNativeCodeSandbox implements CodeSandbox {
         String compileCmd = String.format("javac -encoding utf-8 %s", userCodeFile.getAbsolutePath()); // 编译命令
         try {
             Process compileProcess = Runtime.getRuntime().exec(compileCmd);
-            // 等待Process程序执行完，获取错误码
-            int exitValue = compileProcess.waitFor();
-
-            if (exitValue == 0) {  // 正常退出
-                System.out.println("编译成功");
-                // 通过进程获取正常输出到控制台的信息
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(compileProcess.getInputStream()));
-                StringBuilder compileOutputStringBuilder = new StringBuilder();
-                // 逐行读取
-                String compileOutputLine;
-                while ((compileOutputLine = bufferedReader.readLine()) != null) {
-                    compileOutputStringBuilder.append(compileOutputLine);
-                }
-                System.out.println(compileOutputStringBuilder); // 输出控制台信息
-
-            } else { // 异常退出
-                System.out.println("编译失败，错误码：" + exitValue);
-                // 通过进程获取正常输出到控制台的信息
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(compileProcess.getInputStream()));
-                StringBuilder compileOutputStringBuilder = new StringBuilder();
-                // 逐行读取
-                String compileOutputLine;
-                while ((compileOutputLine = bufferedReader.readLine()) != null) {
-                    compileOutputStringBuilder.append(compileOutputLine);
-                }
-                // 分批获取进程的错误输出
-                BufferedReader errorBufferReader = new BufferedReader(new InputStreamReader(compileProcess.getErrorStream()));
-                StringBuilder errorCompileOutputStringBuilder = new StringBuilder();
-                // 逐行读取错误信息
-                String errorCompileOutputLine;
-                while ((errorCompileOutputLine = errorBufferReader.readLine()) != null) {
-                    errorCompileOutputStringBuilder.append(errorCompileOutputLine);
-                }
-                System.out.println(compileOutputStringBuilder); // 打印正常输出信息
-                System.out.println(errorCompileOutputStringBuilder); // 打印异常输出信息
-            }
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+            ExecuteMessage executeMessage = ProcessUtils.runProcessAndGetMessage(compileProcess, "编译");  // 调用工具类，获取编译信息
+            System.out.println(executeMessage);
+        }catch (Exception e) {
+            e.printStackTrace();
         }
+
         //  3. 执行代码，得到输出结果
         //  4. 收集整理输出结果
         //  5. 文件清理，释放空间
--- src/main/java/com/karlexyan/yojcodesandbox/utils/ProcessUtils.java
+++ src/main/java/com/karlexyan/yojcodesandbox/utils/ProcessUtils.java
@@ -2,6 +2,10 @@ package com.karlexyan.yojcodesandbox.utils;
 
 import com.karlexyan.yojcodesandbox.model.ExecuteMessage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 进程工具类
  */
@@ -11,11 +15,47 @@ public class ProcessUtils {
         ExecuteMessage executeMessage = new ExecuteMessage();
 
         try {
+            // 等待Process程序执行完，获取错误码
+            int exitValue = runProcess.waitFor();
+            executeMessage.setExitValue(exitValue);
 
+            if (exitValue == 0) {  // 正常退出
+                System.out.println(processState + "成功");
+                // 通过进程获取正常输出到控制台的信息
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(runProcess.getInputStream()));
+                StringBuilder compileOutputStringBuilder = new StringBuilder();
+                // 逐行读取
+                String compileOutputLine;
+                while ((compileOutputLine = bufferedReader.readLine()) != null) {
+                    compileOutputStringBuilder.append(compileOutputLine);
+                }
+                executeMessage.setMessage(compileOutputStringBuilder.toString());  // 填入控制台信息
+
+            } else { // 异常退出
+                System.out.println(processState + "失败，错误码：" + exitValue);
+                // 通过进程获取正常输出到控制台的信息
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(runProcess.getInputStream()));
+                StringBuilder compileOutputStringBuilder = new StringBuilder();
+                // 逐行读取
+                String compileOutputLine;
+                while ((compileOutputLine = bufferedReader.readLine()) != null) {
+                    compileOutputStringBuilder.append(compileOutputLine);
+                }
+                executeMessage.setMessage(compileOutputStringBuilder.toString());  // 填入控制台信息
+
+                // 分批获取进程的错误输出
+                BufferedReader errorBufferReader = new BufferedReader(new InputStreamReader(runProcess.getErrorStream()));
+                StringBuilder errorCompileOutputStringBuilder = new StringBuilder();
+                // 逐行读取错误信息
+                String errorCompileOutputLine;
+                while ((errorCompileOutputLine = errorBufferReader.readLine()) != null) {
+                    errorCompileOutputStringBuilder.append(errorCompileOutputLine);
+                }
+                executeMessage.setErrorMessage(errorCompileOutputStringBuilder.toString());  // 填入错误
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return executeMessage;
     }
 }
